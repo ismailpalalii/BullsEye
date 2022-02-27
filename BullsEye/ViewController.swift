466c9926fa70a88328fa580977f15abfc121b390
Ismail Palali <ismail.palali.pp@gmail.com>
2022-02-27 19:51:20 +0300

    Add alert func

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func alert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Hello world", message: "my first ap", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
 }
 
