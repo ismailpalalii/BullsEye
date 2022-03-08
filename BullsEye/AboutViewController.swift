308d034d3904f3f6e38bd17894de9b28a1056e9d
Ismail Palali <ismail.palali.pp@gmail.com>
2022-03-08 22:35:46 +0300

    Add about view controller

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by ismail palali on 8.03.2022.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true,completion: nil)
+    }
+}
